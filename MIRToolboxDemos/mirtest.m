518ec236ae871c4caded6a56e7ad1cbe63dd291c
Olivier Lartillot <olartillot@gmail.com>
2009-10-16 10:58:32 +0000

    Reuse a previous (more complete) mirtest, and adapt to new mirfeatures

--- MIRToolboxDemos/mirtest.m
+++ MIRToolboxDemos/mirtest.m
@@ -23,15 +23,16 @@ close all
 
 f = mirfeatures(audio);
 sf = mirstat(f);
+
 f.dynamics.rms{1}
-f.rhythm.fluctuation.peak{1}
-f.rhythm.fluctuation.centroid{1}
+f.fluctuation.peak{1}
+f.fluctuation.centroid{1}
 f.rhythm.tempo{:}
 f.rhythm.attack.time{:}
 f.rhythm.attack.slope{1}
 sf.dynamics.rms
-sf.rhythm.fluctuation.peak
-sf.rhythm.fluctuation.centroid
+sf.fluctuation.peak
+sf.fluctuation.centroid
 sf.rhythm.tempo
 sf.rhythm.attack.time
 sf.rhythm.attack.slope
@@ -40,58 +41,58 @@ sf.rhythm.attack.slope
 close all
 
 f.timbre.zerocross{:}
-f.timbre.centroid{:}
-f.timbre.brightness{:}
-f.timbre.spread{:}
-f.timbre.skewness{:}
-f.timbre.kurtosis{:}
-f.timbre.rolloff95{:}
-f.timbre.rolloff85{:}
-f.timbre.spectentropy{:}
-f.timbre.flatness{:}
+f.spectral.centroid{:}
+f.spectral.brightness{:}
+f.spectral.spread{:}
+f.spectral.skewness{:}
+f.spectral.kurtosis{:}
+f.spectral.rolloff95{:}
+f.spectral.rolloff85{:}
+f.spectral.spectentropy{:}
+f.spectral.flatness{:}
 sf.timbre.zerocross
-sf.timbre.centroid
-sf.timbre.brightness
-sf.timbre.spread
-sf.timbre.skewness
-sf.timbre.kurtosis
-sf.timbre.rolloff95
-sf.timbre.rolloff85
-sf.timbre.spectentropy
-sf.timbre.flatness
+sf.spectral.centroid
+sf.spectral.brightness
+sf.spectral.spread
+sf.spectral.skewness
+sf.spectral.kurtosis
+sf.spectral.rolloff95
+sf.spectral.rolloff85
+sf.spectral.spectentropy
+sf.spectral.flatness
 
 %pause
 close all
 
-f.timbre.roughness{:}
-f.timbre.irregularity{:}
-f.timbre.inharmonicity{:}
-f.timbre.mfcc{:}
-f.timbre.dmfcc{:}
-f.timbre.ddmfcc{:}
+f.spectral.roughness{:}
+f.spectral.irregularity{:}
+%f.timbre.inharmonicity{:}
+f.spectral.mfcc{:}
+f.spectral.dmfcc{:}
+f.spectral.ddmfcc{:}
 f.timbre.lowenergy{:}
-sf.timbre.roughness
-sf.timbre.irregularity
-sf.timbre.inharmonicity
-sf.timbre.mfcc
-sf.timbre.dmfcc
-sf.timbre.ddmfcc
+sf.spectral.roughness
+sf.spectral.irregularity
+%sf.timbre.inharmonicity
+sf.spectral.mfcc
+sf.spectral.dmfcc
+sf.spectral.ddmfcc
 sf.timbre.lowenergy
 
 %pause
 close all
 
 f.timbre.spectralflux{:}
-f.pitch.salient{:}
-f.pitch.chromagram.peak{:}
-f.pitch.chromagram.centroid{:}
+%f.pitch.salient{:}
+f.tonal.chromagram.peak{:}
+f.tonal.chromagram.centroid{:}
 f.tonal.keyclarity{:}
 f.tonal.mode{:}
 f.tonal.hcdf{:}
 sf.timbre.spectralflux
-sf.pitch.salient
-sf.pitch.chromagram.peak
-sf.pitch.chromagram.centroid
+%sf.pitch.salient
+sf.tonal.chromagram.peak
+sf.tonal.chromagram.centroid
 sf.tonal.keyclarity
 sf.tonal.mode
 sf.tonal.hcdf
@@ -128,167 +129,45 @@ f = mirfeatures(fb);
 %sf = mirstat(f);
 f = mireval(f,audio)
 
-f.dynamics.rms
-f.rhythm.fluctuation.peak
-f.rhythm.fluctuation.centroid
-f.rhythm.tempo
-f.rhythm.attack.time
-f.rhythm.attack.slope
-%sf.dynamics.rms
-%sf.rhythm.fluctuation.peak
-%sf.rhythm.fluctuation.centroid
-%sf.rhythm.tempo
-%sf.rhythm.attack.time
-%sf.rhythm.attack.slope
+f.dynamics.rms{1}
+f.fluctuation.peak{1}
+f.fluctuation.centroid{1}
+f.rhythm.tempo{:}
+f.rhythm.attack.time{:}
+f.rhythm.attack.slope{1}
 
 %pause
 close all
 
-f.timbre.zerocross
-f.timbre.centroid
-f.timbre.brightness
-f.timbre.spread
-f.timbre.skewness
-f.timbre.kurtosis
-f.timbre.rolloff95
-f.timbre.rolloff85
-f.timbre.spectentropy
-%sf.timbre.zerocross
-%sf.timbre.centroid
-%sf.timbre.brightness
-%sf.timbre.spread
-%sf.timbre.skewness
-%sf.timbre.kurtosis
-%sf.timbre.rolloff95
-%sf.timbre.rolloff85
-%sf.timbre.spectentropy
+f.timbre.zerocross{:}
+f.spectral.centroid{:}
+f.spectral.brightness{:}
+f.spectral.spread{:}
+f.spectral.skewness{:}
+f.spectral.kurtosis{:}
+f.spectral.rolloff95{:}
+f.spectral.rolloff85{:}
+f.spectral.spectentropy{:}
+f.spectral.flatness{:}
 
 %pause
 close all
 
-f.timbre.flatness
-f.timbre.roughness
-f.timbre.irregularity
-f.timbre.inharmonicity
-f.timbre.mfcc
-f.timbre.dmfcc
-f.timbre.ddmfcc
-f.timbre.lowenergy
-%sf.timbre.flatness
-%sf.timbre.roughness
-%sf.timbre.irregularity
-%sf.timbre.inharmonicity
-%sf.timbre.mfcc
-%sf.timbre.dmfcc
-%sf.timbre.ddmfcc
-%sf.timbre.lowenergy
+f.spectral.roughness{:}
+f.spectral.irregularity{:}
+%f.timbre.inharmonicity{:}
+f.spectral.mfcc{:}
+f.spectral.dmfcc{:}
+f.spectral.ddmfcc{:}
+f.timbre.lowenergy{:}
 
 %pause
 close all
 
-f.timbre.spectralflux
-f.pitch.salient
-f.pitch.chromagram.peak
-f.pitch.chromagram.centroid
-f.tonal.keyclarity
-f.tonal.mode
-f.tonal.hcdf
-%sf.timbre.spectralflux
-%sf.pitch.salient
-%sf.pitch.chromagram.peak
-%sf.pitch.chromagram.centroid
-%sf.tonal.keyclarity
-%sf.tonal.mode
-%sf.tonal.hcdf
-
-%pause
-close all
-
-%%
-[s,n] = mirsegment(audio,'KernelSize',64)
-f = mirfeatures(s)
-%sf = mirstat(f);
-
-f.dynamics.rms
-f.rhythm.fluctuation.peak
-f.rhythm.fluctuation.centroid
-f.rhythm.tempo
-f.rhythm.attack.time
-f.rhythm.attack.slope
-%sf.dynamics.rms
-%sf.rhythm.fluctuation.peak
-%sf.rhythm.fluctuation.centroid
-%sf.rhythm.tempo
-%sf.rhythm.attack.time
-%sf.rhythm.attack.slope
-
-%pause
-close all
-
-f.timbre.zerocross
-f.timbre.centroid
-f.timbre.brightness
-f.timbre.spread
-f.timbre.skewness
-f.timbre.kurtosis
-f.timbre.rolloff95
-f.timbre.rolloff85
-f.timbre.spectentropy
-%sf.timbre.zerocross
-%sf.timbre.centroid
-%sf.timbre.brightness
-%sf.timbre.spread
-%sf.timbre.skewness
-%sf.timbre.kurtosis
-%sf.timbre.rolloff95
-%sf.timbre.rolloff85
-%sf.timbre.spectentropy
-
-%pause
-close all
-
-f.timbre.flatness
-f.timbre.roughness
-f.timbre.irregularity
-f.timbre.inharmonicity
-f.timbre.mfcc
-f.timbre.dmfcc
-f.timbre.ddmfcc
-f.timbre.lowenergy
-%sf.timbre.flatness
-%sf.timbre.roughness
-%sf.timbre.irregularity
-%sf.timbre.inharmonicity
-%sf.timbre.mfcc
-%sf.timbre.dmfcc
-%sf.timbre.ddmfcc
-%sf.timbre.lowenergy
-
-%pause
-close all
-
-f.timbre.spectralflux
-f.pitch.salient
-f.pitch.chromagram.peak
-f.pitch.chromagram.centroid
-f.tonal.keyclarity
-f.tonal.mode
-f.tonal.hcdf
-%sf.timbre.spectralflux
-%sf.pitch.salient
-%sf.pitch.chromagram.peak
-%sf.pitch.chromagram.centroid
-%sf.tonal.keyclarity
-%sf.tonal.mode
-%sf.tonal.hcdf
-
-%pause
-close all
-
-%%
-%%pause
-%clear f n
-%close all
-%
-%fb = mirfilterbank(s,'NbChannels',5)
-%f = mirfeatures(fb)
+f.timbre.spectralflux{:}
+%f.pitch.salient{:}
+f.tonal.chromagram.peak{:}
+f.tonal.chromagram.centroid{:}
+f.tonal.keyclarity{:}
+f.tonal.mode{:}
+f.tonal.hcdf{:}
