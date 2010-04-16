f07dbd1dfcd0aca3a80fb872511f6765e3b54fe1
Olivier Lartillot <olartillot@gmail.com>
2010-04-16 08:30:30 +0000

    1.2.5 ready

--- MIRToolbox/Contents.m
+++ MIRToolbox/Contents.m
@@ -1,5 +1,5 @@
 % MIRtoolbox
-% Version 1.2.4 17-March-2010
+% Version 1.2.5 16-April-2010
 %
 % A more detailed documentation of each function is available using the
 % help command. For instance, type help miraudio.
