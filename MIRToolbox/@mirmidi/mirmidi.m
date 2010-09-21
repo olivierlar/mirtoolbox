2394f588d5f6e652ab1d73948638f0af0321dee9
Olivier Lartillot <olartillot@gmail.com>
2010-09-21 09:12:04 +0000

    mirmidi keeps multichannel

--- MIRToolbox/@mirmidi/mirmidi.m
+++ MIRToolbox/@mirmidi/mirmidi.m
@@ -30,26 +30,43 @@ function m = main(x,option,postoption)
 if iscell(x) %not(isamir(x,'mirmidi'))
     o = x{1};
     a = x{2};
-    s = mirsegment(a,o);
-    x = mirpitch(s,'Contrast',option.thr,'Sum',0);
-end
-d = get(x,'Data');
-fp = get(x,'FramePos');
-nmat = cell(1,length(d));
-for i = 1:length(d)
-    nmat{i} = [];
-    for j = 1:length(d{i})
-        tij = fp{i}{j}(1);
-        dij = diff(fp{i}{j});
-        for k = 1:size(d{i}{j},3)
-            for l = 1:size(d{i}{j},2)
-                for n = 1:length(d{i}{j}{1,l,k})
-                    f = d{i}{j}{1,l,k}(n);
-                    p = round(hz2midi(f));
-                    nmat{i} = [nmat{i}; tij dij 1 p 120 tij dij];
-                end
+    da = get(a,'Data');
+    nchannels = size(da{1}{1},3);
+    nmat = cell(1,length(da));
+    for c = 1:nchannels
+        da = get(a,'Data');
+        do = get(o,'Data');
+        for i = 1:length(da)
+            for j = 1:length(da{i})
+                da{i}{j} = da{i}{j}(:,:,c);
+                do{i}{j} = do{i}{j}(:,:,c);
             end
         end
+        ac = set(a,'Data',da);
+        oc = set(o,'Data',do);
+        s = mirsegment(ac,oc);
+        x = mirpitch(s,'Contrast',option.thr);
+        d = get(x,'Data');
+        fp = get(x,'FramePos');
+        for i = 1:length(d)
+            for j = 1:length(d{i})
+                tij = fp{i}{j}(1);
+                dij = diff(fp{i}{j});
+                for k = 1:size(d{i}{j},3)
+                    for l = 1:size(d{i}{j},2)
+                        for n = 1:length(d{i}{j}{1,l,k})
+                            f = d{i}{j}{1,l,k}(n);
+                            p = round(hz2midi(f));
+                            nmat{i} = [nmat{i}; tij dij c p 120 tij dij];
+                        end
+                    end
+                end
+            end
+            if c == nchannels
+                nmat{i} = sortrows(nmat{i});
+            end
+        end
+        nmat
     end
 end
 m.data = nmat;
