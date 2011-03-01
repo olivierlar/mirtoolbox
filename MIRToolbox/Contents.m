e8efd0d6a6378ccb4d0d546e18fde8da96815b59
Olivier Lartillot <olartillot@gmail.com>
2011-03-01 08:59:28 +0000

    1.3.2.1 for MTTB-MEM

--- MIRToolbox/Contents.m
+++ MIRToolbox/Contents.m
@@ -1,5 +1,5 @@
 % MIRtoolbox
-% Version 1.3.2 19-January-2011
+% Version 1.3.2.1 1-March-2011
 %
 % A more detailed documentation of each function is available using the
 % help command. For instance, type help miraudio.
