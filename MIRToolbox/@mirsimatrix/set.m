a459507d5813eb692e0036417da565bf02a01b69
Olivier Lartillot <olartillot@gmail.com>
2012-09-25 10:38:24 +0000

    @mirnovelty

--- MIRToolbox/@mirsimatrix/set.m
+++ MIRToolbox/@mirsimatrix/set.m
@@ -3,29 +3,12 @@ function mm = set(m,varargin)
 % and return the updated object
 
 propertyArgIn = varargin;
-graph = m.graph;
-branch = m.branch;
-warp = m.warp;
 d = mirdata(m);
 while length(propertyArgIn) >= 2,
    prop = propertyArgIn{1};
    val = propertyArgIn{2};
    propertyArgIn = propertyArgIn(3:end);
-   switch prop
-       case 'Graph'
-           graph = val;
-       case 'Branch'
-           branch = val;
-       case 'Warp'
-           warp = val;
-       otherwise
-           d = set(d,prop,val);
-   end
+   d = set(d,prop,val);
 end
-mm.diagwidth = m.diagwidth;
-mm.view = m.view;
-mm.similarity = m.similarity;
-mm.graph = graph;
-mm.branch = branch;
-mm.warp = warp;
-mm = class(mm,'mirsimatrix',d);
+mm.method = m.method;
+mm = class(mm,'mirnovelty',d);
