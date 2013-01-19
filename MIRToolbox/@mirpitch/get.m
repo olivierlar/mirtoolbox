74376e1f1d0e3a0d44f3d40935268b222ac2c38d
Olivier Lartillot <olartillot@gmail.com>
2013-01-19 22:51:41 +0000

    @mirmetre

--- MIRToolbox/@mirpitch/get.m
+++ MIRToolbox/@mirpitch/get.m
@@ -1,18 +1,8 @@
-function val = get(p, propName)
-% GET Get properties from the MIRpitch object
+function val = get(m, propName)
+% GET Get properties from the MIRmetre object
 % and return the value
 
 switch propName
-    case 'Amplitude'
-        val = p.amplitude;
-    case 'Start'
-        val = p.start;
-    case 'End'
-        val = p.end;
-    case 'Mean'
-        val = p.mean;
-    case 'Degrees'
-        val = p.degrees;
-    otherwise
-        val = get(mirscalar(p),propName);
+    case 'Meters'
+        val = m.meters;
 end
