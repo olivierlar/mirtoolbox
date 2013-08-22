915246cfb7070590a5f51d559a5d0bfc255030a2
Olivier Lartillot <olartillot@gmail.com>
2013-08-22 14:04:16 +0000

    mirnovelty: 'KernelSize' also called 'Kernel'; by default 'Kernel' set to 0, corresponding to previous 'New'; 'Granul' removed; new 'Standard' syntax.

--- MIRToolbox/mirnovelty.m
+++ MIRToolbox/mirnovelty.m
@@ -1,6 +1,16 @@
 function varargout = mirnovelty(orig,varargin)
-%   n = mirnovelty(m) evaluates the novelty score from a similarity matrix.
-%   [n,m] = mirnovelty(m) also return the similarity matrix.
+% n = mirnovelty(m) evaluates the novelty score from a similarity matrix.
+% By default, if the 'Kernel' (or 'KernelSize') option is not specified, 
+%   our new multi-granular strategy for novelty computation is used. It is 
+%   a simpler but more powerful and general method that automatically 
+%   detects homogeneous segments of any size.
+%
+% When this default multi-granular approach is used for academic research, 
+%   please cite the following publication:
+% Lartillot, O., Cereghetti, D., Eliard, K., Grandjean, D., ?A simple, 
+%   high-yield method for assessing structural novelty?, International 
+%   Conference on Music and Emotion, Jyväskylä, 2013.
+%
 %   Optional argument: 
 %       mirnovelty(...,'Distance',f) specifies the name of a dissimilarity
 %           distance function, from those proposed in the Statistics Toolbox
@@ -11,13 +21,15 @@ function varargout = mirnovelty(orig,varargin)
 %           distance function. cf. mirsimatrix 
 %           default value: f = 'exponential'
 %               corresponding to f(x) = exp(-x)
-%       mirnovelty(...,'KernelSize',s) or more simply mirnovelty(...,s) 
+%       mirnovelty(...,'KernelSize',s) selects the kernel-based method and
 %           specifies the length of the gaussian kernel, in samples.
 %           default value: s = 64.
 %       mirnovelty(...,'Normal',0) does not normalize the novelty curve
 %           between the values 0 and 1.
 %       mirnovelty(...,'Horizontal') uses the 'Horizontal' option in
 %           mirsimatrix instead of 'TimeLag' used here by default.
+%
+%   [n,m] = mirnovelty(m) also return the similarity matrix.
 %
 %   Foote, J. & Cooper, M. (2003). Media Segmentation using Self-Similarity
 % Decomposition,. In Proc. SPIE Storage and Retrieval for Multimedia
@@ -33,30 +45,25 @@ function varargout = mirnovelty(orig,varargin)
         sm.default = 'exponential';
     option.sm = sm;
 
-        K.key = {'KernelSize','Width'};
+        K.key = {'KernelSize','Width','Kernel'};
         K.type = 'Integer';
-        K.default = NaN;
+        K.default = Inf;
     option.K = K;
     
-        gran.key = 'Granul';
-        gran.type = 'Integer';
-        gran.default = 1;
-    option.gran = gran;
+%        gran.key = 'Granul';
+%        gran.type = 'Integer';
+%        gran.default = 1;
+%    option.gran = gran;
 
         transf.type = 'String';
-        transf.default = 'TimeLag';
-        transf.choice = {'Horizontal','TimeLag'};
+        transf.default = 'Standard';
+        transf.choice = {'Horizontal','TimeLag','Standard'};
     option.transf = transf;
 
         flux.key = 'Flux';
         flux.type = 'Boolean';
         flux.default = 0;
     option.flux = flux;
-
-        new.key = 'New';
-        new.type = 'Boolean';
-        new.default = 0;
-    option.new = new;
 
         simple.key = 'Simple';
         simple.type = 'Boolean';
@@ -65,7 +72,7 @@ function varargout = mirnovelty(orig,varargin)
 
         half.key = 'Half';
         half.type = 'Boolean';
-        half.default = 0;
+        half.default = 1;
     option.half = half;
 
         cluster.key = 'Cluster';
@@ -106,17 +113,14 @@ function [x type] = init(x,option)
 type = 'mirscalar';
 if ~option.simple && ...
         not(isamir(x,'mirscalar') && strcmp(get(x,'Title'),'Novelty'))
-    if isnan(option.K)
-        if option.cluster || option.flux || option.new
-            option.K = Inf; %150;
-        else
-            option.K = 16; %64;
+    
+    if ~isinf(option.K)
+        option.half = 0;
+        if strcmpi(option.transf,'Standard')
+            option.transf = 'TimeLag';
         end
     end
-    if option.cluster || option.flux || option.new
-        option.transf = 'Standard';
-        option.half = 1;
-    end
+    
     x = mirsimatrix(x,'Distance',option.dist,'Similarity',option.sm,...
                       'Width',max(option.K),option.transf,...
                       'Half',option.half,'Cluster',option.cluster,...
@@ -150,191 +154,186 @@ elseif option.simple
     end
     n = mirscalar(orig,'Data',score,'Title','Novelty'); 
 
-else
-    if option.new
-        s = get(orig,'Data');
-        %dw = 0; %40;
-        for i = 1:length(s)
-            for j = 1:length(s{i})
-                %sij0 = s{i}{j}(:);
-                %sij0(sij0 == 1) = [];
-                %Msij = max(sij0);
-                %msij = min(sij0);
-                %k0 = 1;
-                score{i}{j} = zeros(1,size(s{i}{j},2));
-                for k = 3:size(s{i}{j},2)
-                    %delt = min(k-2,dw+1);
-                    %dist = NaN(1,delt);
-                    %for l = 1:delt
-                    l = 1;
-                        bloc = s{i}{j}(k-l-1:-1:1,1:k-l);
-                        stop = find(s{i}{j}(k-l-1:-1:1,k) ...
-                                    > min(s{i}{j}(k-l-1:-1:1,k-l),...
-                                          max(mean(bloc,2) - 2 * std(bloc,0,2),...
-                                              min(bloc,[],2))));
-                        if length(stop) < 2
+elseif isinf(option.K)
+    s = get(orig,'Data');
+    %dw = 0; %40;
+    for i = 1:length(s)
+        for j = 1:length(s{i})
+            %sij0 = s{i}{j}(:);
+            %sij0(sij0 == 1) = [];
+            %Msij = max(sij0);
+            %msij = min(sij0);
+            %k0 = 1;
+            score{i}{j} = zeros(1,size(s{i}{j},2));
+            for k = 3:size(s{i}{j},2)
+                %delt = min(k-2,dw+1);
+                %dist = NaN(1,delt);
+                %for l = 1:delt
+                l = 1;
+                    bloc = s{i}{j}(k-l-1:-1:1,1:k-l);
+                    stop = find(s{i}{j}(k-l-1:-1:1,k) ...
+                                > min(s{i}{j}(k-l-1:-1:1,k-l),...
+                                      max(mean(bloc,2) - 2 * std(bloc,0,2),...
+                                          min(bloc,[],2))));
+                    if length(stop) < 2
+                        stop = k-l;
+                    else
+                        stop(end+1) = stop(end)+1;
+                        stopp = find(diff(stop(1:end-1)) == 1 & diff(stop,2) == 0,1);
+                        if isempty(stopp)
                             stop = k-l;
                         else
-                            stop(end+1) = stop(end)+1;
-                            stopp = find(diff(stop(1:end-1)) == 1 & diff(stop,2) == 0,1);
-                            if isempty(stopp)
-                                stop = k-l;
-                            else
-                                stop = stop(stopp);
-                            end
+                            stop = stop(stopp);
                         end
-                        if ~isempty(stop)
-                            %dist(l) = ... stop * ...
-                              dist = sum(diff([s{i}{j}(k-l-stop+1:k-l-1,k),...
-                                        s{i}{j}(k-l-stop+1:k-l-1,k-l)],...
-                                       1,2));% ...
-                                          %.*((s{i}{j}(k-l-stop:k-l-1,k-1) - msij)...
-                                          %   /(Msij-msij)).^1); ...
-                                      %/ max(k-1, 100);
-                        end
-                        %if l == 1
-                        %    dist0 = dist(l);
-                        %end
-                        %if l > 1 && dist(l) > dist(l0) && k-l >= k0
-                        %    dist(l0) = dist(l);
-                        %    dist(l) = 0;
-                        %else
-                        %    l0 = l;
-                        %    if l > 1 && k-l >= k0
-                        %        dist(l0) = dist0;
-                        %        k0 = k-1;
-                        %    end
-                        %end
+                    end
+                    if ~isempty(stop)
+                        %dist(l) = ... stop * ...
+                          dist = sum(diff([s{i}{j}(k-l-stop+1:k-l-1,k),...
+                                    s{i}{j}(k-l-stop+1:k-l-1,k-l)],...
+                                   1,2));% ...
+                                      %.*((s{i}{j}(k-l-stop:k-l-1,k-1) - msij)...
+                                      %   /(Msij-msij)).^1); ...
+                                  %/ max(k-1, 100);
+                    end
+                    %if l == 1
+                    %    dist0 = dist(l);
                     %end
-                    %best = 0;
-                    %for l = 1:delt
-                        %dist(l) = dist(l) + (l-1)/(dw+1);
-                    %    if dist(l) > best
-                    %        best = dist(l);
-                    %        bestl = l;
+                    %if l > 1 && dist(l) > dist(l0) && k-l >= k0
+                    %    dist(l0) = dist(l);
+                    %    dist(l) = 0;
+                    %else
+                    %    l0 = l;
+                    %    if l > 1 && k-l >= k0
+                    %        dist(l0) = dist0;
+                    %        k0 = k-1;
                     %    end
                     %end
-                    score{i}{j}(k) = max(dist,0); %best;
-                    %if bestl > 1
-                    %    score{i}{j}(k-1:k-bestl+1) = ...
-                    %        min(score{i}{j}(k-1:k-bestl+1),best);
-                    %end
-                end
+                %end
+                %best = 0;
+                %for l = 1:delt
+                    %dist(l) = dist(l) + (l-1)/(dw+1);
+                %    if dist(l) > best
+                %        best = dist(l);
+                %        bestl = l;
+                %    end
+                %end
+                score{i}{j}(k) = max(dist,0); %best;
+                %if bestl > 1
+                %    score{i}{j}(k-1:k-bestl+1) = ...
+                %        min(score{i}{j}(k-1:k-bestl+1),best);
+                %end
             end
-        end
-        n = mirscalar(orig,'Data',score,'Title','Novelty'); 
-
-    elseif option.flux
-        fl = mirflux(orig);
-        if 1
-            n = mirscalar(fl,'Title','Novelty');
-        else
-            score = get(fl,'Data');
-            dw = get(orig,'DiagWidth');
-            if dw < Inf && not(isempty(postoption)) && postoption.normal
-                for k = 1:length(score)
-                    for l = 1:length(score{k})
-                        lg = length(score{k}{l});
-                        for i = 1:lg
-                            score{k}{l}(i) = score{k}{l}(i) * ...
-                                (1 + atan( dw/min(i,dw) - 1)) / (1 + atan(dw-1));
-                        end
-                    end
-                end
-            end
-            n = mirscalar(fl,'Data',score,'Title','Novelty');
-        end
-    else
-
-        %fp = get(orig,'FramePos');
-        if not(isa(orig,'mirscalar'))
-            s = get(orig,'Data');
-            cl = get(orig,'Clusters');
-            if isempty(cl)
-                dw = get(orig,'DiagWidth');
-                Ks = option.K;
-                for k = 1:length(s)
-                    for i = 1:length(Ks)
-                        if isnumeric(dw)
-                            dwk = dw;
-                        else
-                            dwk = dw{k};
-                        end
-                        if ~isnan(Ks(i)) && Ks(i)
-                            cgs = min(Ks(i),dwk);
-                        else
-                            cgs = dwk;
-                        end
-                        cg = checkergauss(cgs,option.transf)/cgs.^option.gran;
-                        disp('Computing convolution, please wait...')
-                        for z = 1:length(s{k})
-                            sz = s{k}{z};
-                            szma = max(max(sz));
-                            szmi = min(min(sz));
-                            sz = (sz-szmi)/(szma-szmi);
-                            sz = 2*sz-1;
-                            sz(isnan(sz)) = 0;
-                            cv = convolve2(sz,cg,'same');
-                            nl = size(cv,1);
-                            nc = size(cv,2);
-                            if nl == 0
-                                warning('WARNING IN NOVELTY: No frame decomposition. The novelty score cannot be computed.');
-                                score{k}{z} = [];
-                            else
-                                sco = cv(floor(size(cv,1)/2),:);
-                                incr = find(diff(sco)>=0);
-                                if not(isempty(incr))
-                                    decr = find(diff(sco)<=0);
-                                    sco(1:incr(1)-1) = NaN(1,incr(1)-1);
-                                    if not(isempty(decr))
-                                        sco(decr(end)+1:end) = NaN(1,length(sco)-decr(end));
-                                    end
-                                    incr = find(diff(sco)>=0);
-                                    sco2 = sco;
-                                    if not(isempty(incr))
-                                        sco2 = sco2(1:incr(end)+1);
-                                    end
-                                    decr = find(diff(sco)<=0);
-                                    if not(isempty(decr)) && decr(1)>2
-                                        sco2 = sco2(decr(1)-1:end);
-                                    end
-                                    mins = min(sco2);
-                                    rang = find(sco>= mins);
-                                    if not(isempty(rang))
-                                        sco(1:rang(1)-1) = NaN(1,rang(1)-1);
-                                        sco(rang(end)+1:end) = NaN(1,length(sco)-rang(end));
-                                    end
-                                end
-                                score{k}{z}(i,:) = sco';
-                            end
-                        end
-                    end
-                end
-            else
-                score = cell(1,length(s));
-                for k = 1:length(s)
-                    score{k} = cell(1,length(s{k}));
-                    for z = 1:length(s{k})
-                        score{k}{z} = zeros(1,size(cl{k}{z},1));
-                        for i = 1:length(cl{k}{z})
-                            for j = 1:length(cl{k}{z})
-                                clij = cl{k}{z}(i,j);
-                                if ~isnan(clij) %&& clij > .9
-                                    score{k}{z}(i) = max(score{k}{z}(i), ...
-                                        clij^5 * j^2);
-                                    score{k}{z}(i+j) = max(score{k}{z}(i+j), ...
-                                        clij^5 * j^2);
-                                end
-                            end
-                        end
-                        %fp{k}{z} = [fp{k}{z}(1,2:end);fp{k}{z}(2,1:end-1)];
-                    end
-                end
-            end
-        else
-            score = get(orig,'Data');
         end
     end
+    n = mirscalar(orig,'Data',score,'Title','Novelty'); 
+
+elseif option.flux
+    fl = mirflux(orig);
+    if 1
+        n = mirscalar(fl,'Title','Novelty');
+    else
+        score = get(fl,'Data');
+        dw = get(orig,'DiagWidth');
+        if dw < Inf && not(isempty(postoption)) && postoption.normal
+            for k = 1:length(score)
+                for l = 1:length(score{k})
+                    lg = length(score{k}{l});
+                    for i = 1:lg
+                        score{k}{l}(i) = score{k}{l}(i) * ...
+                            (1 + atan( dw/min(i,dw) - 1)) / (1 + atan(dw-1));
+                    end
+                end
+            end
+        end
+        n = mirscalar(fl,'Data',score,'Title','Novelty');
+    end
+
+elseif ~isa(orig,'mirscalar')
+    s = get(orig,'Data');
+    cl = get(orig,'Clusters');
+    if isempty(cl)
+        dw = get(orig,'DiagWidth');
+        Ks = option.K;
+        for k = 1:length(s)
+            for i = 1:length(Ks)
+                if isnumeric(dw)
+                    dwk = dw;
+                else
+                    dwk = dw{k};
+                end
+                if ~isnan(Ks(i)) && Ks(i)
+                    cgs = min(Ks(i),dwk);
+                else
+                    cgs = dwk;
+                end
+                cg = checkergauss(cgs,option.transf)/cgs;%.^option.gran;
+                disp('Computing convolution, please wait...')
+                for z = 1:length(s{k})
+                    sz = s{k}{z};
+                    szma = max(max(sz));
+                    szmi = min(min(sz));
+                    sz = (sz-szmi)/(szma-szmi);
+                    sz = 2*sz-1;
+                    sz(isnan(sz)) = 0;
+                    cv = convolve2(sz,cg,'same');
+                    nl = size(cv,1);
+                    nc = size(cv,2);
+                    if nl == 0
+                        warning('WARNING IN NOVELTY: No frame decomposition. The novelty score cannot be computed.');
+                        score{k}{z} = [];
+                    else
+                        sco = cv(floor(size(cv,1)/2),:);
+                        incr = find(diff(sco)>=0);
+                        if not(isempty(incr))
+                            decr = find(diff(sco)<=0);
+                            sco(1:incr(1)-1) = NaN(1,incr(1)-1);
+                            if not(isempty(decr))
+                                sco(decr(end)+1:end) = NaN(1,length(sco)-decr(end));
+                            end
+                            incr = find(diff(sco)>=0);
+                            sco2 = sco;
+                            if not(isempty(incr))
+                                sco2 = sco2(1:incr(end)+1);
+                            end
+                            decr = find(diff(sco)<=0);
+                            if not(isempty(decr)) && decr(1)>2
+                                sco2 = sco2(decr(1)-1:end);
+                            end
+                            mins = min(sco2);
+                            rang = find(sco>= mins);
+                            if not(isempty(rang))
+                                sco(1:rang(1)-1) = NaN(1,rang(1)-1);
+                                sco(rang(end)+1:end) = NaN(1,length(sco)-rang(end));
+                            end
+                        end
+                        score{k}{z}(i,:) = sco';
+                    end
+                end
+            end
+        end
+    else
+        score = cell(1,length(s));
+        for k = 1:length(s)
+            score{k} = cell(1,length(s{k}));
+            for z = 1:length(s{k})
+                score{k}{z} = zeros(1,size(cl{k}{z},1));
+                for i = 1:length(cl{k}{z})
+                    for j = 1:length(cl{k}{z})
+                        clij = cl{k}{z}(i,j);
+                        if ~isnan(clij) %&& clij > .9
+                            score{k}{z}(i) = max(score{k}{z}(i), ...
+                                clij^5 * j^2);
+                            score{k}{z}(i+j) = max(score{k}{z}(i+j), ...
+                                clij^5 * j^2);
+                        end
+                    end
+                end
+                %fp{k}{z} = [fp{k}{z}(1,2:end);fp{k}{z}(2,1:end-1)];
+            end
+        end
+    end
+else
+    score = get(orig,'Data');
 end
 
 if not(isempty(postoption))
@@ -383,7 +382,7 @@ y = {n orig};
 
 function y = checkergauss(N,transf)
 hN = ceil(N/2);
-if strcmpi(transf,'TimeLag')
+if strcmpi(transf,'TimeLag') || strcmpi(transf,'Standard')
     y = zeros(2*N,N);
     for j = 1:N
         for i = 1:2*N+1
