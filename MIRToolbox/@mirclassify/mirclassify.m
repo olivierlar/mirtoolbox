0bb6210c5b365bc78a544ccc84d2b2e68b4ed45e
Olivier Lartillot <olartillot@gmail.com>
2014-06-17 08:26:54 +0000

    New mirclassify with leave-one-out cross-validation

--- MIRToolbox/@mirclassify/mirclassify.m
+++ MIRToolbox/@mirclassify/mirclassify.m
@@ -1,22 +1,4 @@
-function c = mirclassify(a,da,t,dt,varargin)
-%   c = mirclassify(test,features_test,train,features_train) classifies the
-%       audio sequence(s) contained in the audio object test, along the
-%       analytic feature(s) features_test, following the supervised
-%       learning of a training set defined by the audio object train and
-%       the corresponding analytic feature(s) features_train.
-%           * The analytic feature(s) features_test should *not* be frame 
-%               decomposed. Frame-decomposed data should first be 
-%               summarized, using for instance mirmean or mirstd.
-%           * Multiple analytic features have to be grouped into one array 
-%               of cells.
-%       You can also integrate your own arrays of numbers computed outside
-%           MIRtoolbox as part of the features. These arrays should be 
-%           given as matrices where each successive column is the analysis 
-%           of each successive file.
-%   Example:
-%       mirclassify(test, mfcc(test), train, mfcc(train))
-%       mirclassify(test, {mfcc(test), centroid(test)}, ...
-%                train, {mfcc(train), centroid(train)})
+function c = mirclassify(x,varargin)
 %   Optional argument:
 %       mirclassify(...,'Nearest') uses the minimum distance strategy.
 %           (by default)
@@ -32,171 +14,219 @@ function c = mirclassify(a,da,t,dt,varargin)
 %               (cf. help gmm)
 %           Requires the Netlab toolbox.
 
-lab = get(t,'Label');
-c.labtraining = lab;
-rlab = get(a,'Label');
-c.labtest = rlab;
+if not(iscell(x))
+    x = {x};
+end
+n = get(x{1},'Name');
+
+l = length(n);    % Number of training samples
+lab = cell(1,l);
+allabs = struct;
+for i = 1:l
+    sl = strfind(n{i},'/');
+    if isempty(sl)
+        mirerror('mirclassify','...');
+    end
+    lab{i} = n{i}(1:sl(end)-1);
+    if i == 1
+        allabs.name = lab{1};
+        allabs.idx = 1;
+        continue
+    end
+    [test li] = ismember(lab{i},{allabs.name});
+    if test
+        allabs(li).idx(end+1) = i;
+    else
+        allabs(end+1).name = lab{i};
+        allabs(end).idx = i;
+    end
+end
+
 [k,ncentres,covartype,kmiter,emiter,d,mahl] = scanargin(varargin);
-disp('Classifying...')
-
-%% Training phase
-if not(iscell(dt))
-    dt = {dt};
-end
-lvt = length(get(t,'Data'));    % Number of training samples
-vt = [];                        % Preprocessed training vectors
-mn = cell(1,length(dt));
-sd = cell(1,length(dt));
-for i = 1:length(dt)
-    if isnumeric(dt{i})
-        d = cell(1,size(dt{i},2));
-        for j = 1:size(dt{i},2)
-            d{j} = dt{i}(:,j);
+nfolds = length(allabs(1).idx);
+v = [];                        % Preprocessed training vectors
+mn = cell(1,length(x));
+sd = cell(1,length(x));
+for i = 1:length(x)
+    if isnumeric(x{i})
+        d = cell(1,size(x{i},2));
+        for j = 1:size(x{i},2)
+            d{j} = x{i}(:,j);
         end
     else
-        d = get(dt{i},'Data');
+        d = get(x{i},'Data');
     end
     
-    [vt mn{i} sd{i}] = integrate(vt,d,lvt);
+    [v mn{i} sd{i}] = integrate(v,d,l);
     
-    if 0 %isa(dt{i},'scalar')
-        m = mode(dt{i});
+    if 0 %isa(t{i},'scalar')
+        m = mode(x{i});
         if not(isempty(m))
-            vt = integrate(vt,m,lvt);
+            v = integrate(v,m,l);
         end
     end
 end
-c.training = vt;
-dim = size(vt,1);
+mahl = cov(v');
 
-%% Test phase
-if not(iscell(da))
-    da = {da};
+for i = 1:length(allabs)
+    s = RandStream('mt19937ar','Seed','shuffle');
+    p = randperm(s,length(allabs(i).idx));
+    allabs(i).idx = allabs(i).idx(p);
 end
-lva = length(get(a,'Data'));    % Number of test samples
-va = [];                        % Preprocessed test vectors
-for i = 1:length(da)
-    if isnumeric(da{i})
-        d = cell(1,size(da{i},2));
-        for j = 1:size(da{i},2)
-            d{j} = da{i}(:,j);
-        end
-    else
-        d = get(da{i},'Data');
-    end
-    
-    va = integrate(va,d,lva,mn{i},sd{i});
-    
-    if 0 %isa(da{i},'scalar')
-        m = mode(da{i});
-        if not(isempty(m))
-            va = integrate(va,m,lva,m{i},s{i});
-        end
-    end
-end
-c.test = va;
 
-c.nbobs = lvt;
-totva = [vt va];
-mahl = cov(totva');
-if k                % k-Nearest Neighbour
-    c.nbparam = lvt;
-    for l = 1:lva
-        [sv,idx] = sort(distance(va(:,l),vt,d,mahl));
-        labs = cell(0); % Class labels
-        founds = [];    % Number of found elements in each class
-        for i = idx(1:k)
-            labi = lab{i};
+for h = 1:nfolds
+    va = [];
+    vt = [];
+    idxh = zeros(1,length(allabs));
+    rlab = cell(1,length(allabs));
+    rlabt = cell(1,length(allabs)*(nfolds-1));
+    for i = 1:length(allabs)
+        idxi = allabs(i).idx;
+        va = [va v(:,allabs(i).idx(h))];
+        for j = (nfolds-1)*(i-1)+(1:nfolds-1)
+            rlabt{j} = allabs(i).name;
+        end
+        idxi(h) = [];
+        vt = [vt v(:,idxi)];  
+        rlab{i} = allabs(i).name;
+    end
+
+    if k                % k-Nearest Neighbour
+        c.nbparam = size(vt,2);
+        for l = 1:size(va,2)
+            [sv,idx] = sort(distance(va(:,l),vt,mahl));
+            labs = cell(0); % Class labels
+            founds = [];    % Number of found elements in each class
+            for i = idx(1:k)
+                labi = rlabt{i};
+                found = 0;
+                for j = 1:length(labs)
+                    if isequal(labi,labs{j})
+                        found = j;
+                    end
+                end
+                if found
+                    founds(found) = founds(found)+1;
+                else
+                    labs{end+1} = labi;
+                    founds(end+1) = 1;
+                end
+            end
+            [b ib] = max(founds);
+            c.classes{h,l} = labs{ib};
+        end
+    elseif ncentres     % Gaussian Mixture Model
+        labs = cell(0);    % Class labels
+        founds = cell(0);  % Elements associated to each label.
+        for i = 1:size(vt,2)
+            labi = rlabt{i};
             found = 0;
             for j = 1:length(labs)
                 if isequal(labi,labs{j})
-                    found = j;
+                    founds{j}(end+1) = i;
+                    found = 1;
                 end
             end
-            if found
-                founds(found) = founds(found)+1;
-            else
+            if not(found)
                 labs{end+1} = labi;
-                founds(end+1) = 1;
+                founds{end+1} = i;
             end
         end
-        [b ib] = max(founds);
-        c.classes{l} = labs{ib};
-    end
-elseif ncentres     % Gaussian Mixture Model
-    labs = cell(0);    % Class labels
-    founds = cell(0);  % Elements associated to each label.
-    for i = 1:lvt
-        labi = lab{i};
-        found = 0;
-        for j = 1:length(labs)
-            if isequal(labi,labs{j})
-                founds{j}(end+1) = i;
-                found = 1;
-            end
+        options      = zeros(1, 18);
+        options(2:3) = 1e-4;
+        options(4)   = 1e-6;
+        options(16)  = 1e-8;
+        options(17)  = 0.1;
+        options(1)   = 0; %Prints out error values, -1 else
+        c.nbparam = 0;
+        OK = 0;
+        while not(OK)
+            OK = 1;
+            for i = 1:length(labs)
+                options(14)  = kmiter;
+                try
+                    mix{i} = gmm(size(vt,1),ncentres,covartype);
+                catch
+                    error('ERROR IN CLASSIFY: Netlab toolbox not installed.');
+                end
+                mix{i} = netlabgmminit(mix{i},vt(:,founds{i})',options);
+                options(5)   = 1;
+                options(14)  = emiter;
+                try
+                    mix{i} = gmmem(mix{i},vt(:,founds{i})',options);
+                    c.nbparam = c.nbparam + ...
+                        length(mix{i}.centres(:)) + length(mix{i}.covars(:));
+                catch
+                    %err = lasterr;
+                    %warning('WARNING IN CLASSIFY: Problem when calling GMMEM:');
+                    %disp(err);
+                    %disp('Let us try again...');
+                    OK = 0;
+                end
+            end    
         end
-        if not(found)
-            labs{end+1} = labi;
-            founds{end+1} = i;
-        end
-    end
-    options      = zeros(1, 18);
-    options(2:3) = 1e-4;
-    options(4)   = 1e-6;
-    options(16)  = 1e-8;
-    options(17)  = 0.1;
-    options(1)   = 0; %Prints out error values, -1 else
-    c.nbparam = 0;
-    OK = 0;
-    while not(OK)
-        OK = 1;
+        pr = zeros(size(va,2),length(labs));
         for i = 1:length(labs)
-            options(14)  = kmiter;
-            try
-                mix{i} = gmm(dim,ncentres,covartype);
-            catch
-                error('ERROR IN CLASSIFY: Netlab toolbox not installed.');
-            end
-            mix{i} = netlabgmminit(mix{i},vt(:,founds{i})',options);
-            options(5)   = 1;
-            options(14)  = emiter;
-            try
-                mix{i} = gmmem(mix{i},vt(:,founds{i})',options);
-                c.nbparam = c.nbparam + ...
-                    length(mix{i}.centres(:)) + length(mix{i}.covars(:));
-            catch
-                err = lasterr;
-                warning('WARNING IN CLASSIFY: Problem when calling GMMEM:');
-                disp(err);
-                disp('Let us try again...');
-                OK = 0;
-            end
-        end    
-    end
-    pr = zeros(lva,length(labs));
-    for i = 1:length(labs)
-        prior = length(founds{i})/lvt;
-        pr(:,i) = prior * gmmprob(mix{i},va');
-        %c.post{i} = gmmpost(mix{i},va');
-    end
-    [mm ib] = max(pr');
-    for i = 1:lva
-        c.classes{i} = labs{ib(i)};
-    end
-end
-if isempty(rlab)
-    c.correct = NaN;
-else
-    correct = 0;
-    for i = 1:lva
-        if isequal(c.classes{i},rlab{i})
-            correct = correct + 1;
+            prior = length(founds{i})/size(vt,2);
+            pr(:,i) = prior * gmmprob(mix{i},va');
+            %c.post{i} = gmmpost(mix{i},va');
+        end
+        [mm ib] = max(pr');
+        for i = 1:size(va,2)
+            c.classes{h,i} = labs{ib(i)};
         end
     end
-    c.correct = correct / lva;
-end        
+    if isempty(rlab)
+        c.correct = NaN;
+    else
+        correct = 0;
+        for i = 1:length(rlab)
+            if isequal(c.classes{h,i},rlab{i})
+                correct = correct + 1;
+            end
+        end
+        c.correct(h) = correct / length(rlab);
+    end   
+end
+
 c = class(c,'mirclassify');
 
+disp('Expected classes:')
+rlab
+
+disp('Classification results:')
+c.classes
+
+if isnan(c.correct)
+    disp('No label has been associated to the test set. Correct classification rate cannot be computed.');
+else
+    disp(['Correct classification rate: ',num2str(mean(c.correct),2)]);
+end
+
+figure
+if k
+    v0 = zeros(size(v,1),0);
+    for i = 1:length(allabs)
+        v0 = [v0 v(:,allabs(i).idx)];
+    end
+    ltick = nfolds;
+elseif ncentres
+    v0 = zeros(size(v,1),length(mix)*ncentres);
+    for i = 1:length(mix)
+        for j = 1:ncentres
+            v0(:,ncentres*(i-1)+j) = mix{i}.centres(j,:)';
+        end
+    end
+    ltick = ncentres;
+end
+imagesc(v0)
+set(gca,'XTick',ltick+.5:ltick:size(v,2)+.5)
+set(gca,'XTickLabel','')
+set(gca,'YTick',[])
+grid on
+for i = 1:length(allabs)
+    text(ltick*(i-1)+1,1,allabs(i).name);
+end
 
 function [vt m s] = integrate(vt,v,lvt,m,s)
 % lvt is the number of samples
@@ -273,7 +303,7 @@ while i <= length(v)
 end
 
 
-function y = distance(a,t,d,mahl)
+function y = distance(a,t,mahl)
 
 for i = 1:size(t,2)
     if det(mahl) > 0  % more generally, uses cond
