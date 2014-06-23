c6ae59913f4df3dee4baf9375cf2a65c9ea08e32
Olivier Lartillot <olartillot@gmail.com>
2014-06-23 15:24:26 +0000

    mirmetre display improvements

--- MIRToolbox/@mirmetre/display.m
+++ MIRToolbox/@mirmetre/display.m
@@ -14,6 +14,7 @@ for h = 1:length(d)
         if i == 1
             irgb = [0 0 0];
         else
+            continue
             irgb = num2col(i)/2; %shiftdim(1-num2col(i),-1);
         end
         mac = 0;
@@ -33,16 +34,17 @@ for h = 1:length(d)
             micmac = 1;
         end
         for i2 = 1:length(d{h}{1}{i})
+            if isempty(d{h}{1}{i}(i2).globpms)
+                continue
+            end
+            
             if isempty(d{h}{1}{i}(i2).function)
                 %continue
-                irgb2 = .5+irgb/2;
+                irgb2 = .33+.66*irgb;
             else
                 irgb2 = irgb;
             end
             timidx = d{h}{1}{i}(i2).timidx;
-            text(fp{h}{1}(1,timidx(1)),...-1,...
-                 60./d{h}{1}{i}(i2).globpms(1),...
-                 num2str(d{h}{1}{i}(i2).lvl),'FontSize',10,'Color',irgb2)
             for i3 = 1:length(d{h}{1}{i}(i2).score)
                 if i3>1 && length(d{h}{1}{i}(i2).globpms) >= i3
                     plot(fp{h}{1}(1,timidx([i3-1 i3])),...
@@ -50,7 +52,7 @@ for h = 1:length(d)
                          ':','Color',irgb2);
                 end
             end
-            for i3 = 1:length(d{h}{1}{i}(i2).score)
+            for i3 = 1:length(d{h}{1}{i}(i2).globpms)
                 scor = (d{h}{1}{i}(i2).score(i3) - mic) / micmac +1e-16;
                 %rgb = ones(1,1,3) - scor * irgb;
                 plot(fp{h}{1}(1,timidx(i3)),...
@@ -58,6 +60,19 @@ for h = 1:length(d)
                 plot(fp{h}{1}(1,timidx([i3 i3])),...
                     [60./d{h}{1}{i}(i2).globpms(i3) ...
                      60./d{h}{1}{i}(i2).bpms(i3)],'Color',irgb2);
+            end
+            if isempty(d{h}{1}{i}(i2).function)
+                text(fp{h}{1}(1,timidx(1)),...-1,...
+                     60./d{h}{1}{i}(i2).globpms(1),...
+                     num2str(d{h}{1}{i}(i2).lvl,3),'FontSize',10,...
+                     'Color',[0 0 0],...
+                     'BackgroundColor',[1 1 1],'Margin',1)
+            else
+                text(fp{h}{1}(1,timidx(1)),...-1,...
+                     60./d{h}{1}{i}(i2).globpms(1),...
+                     num2str(d{h}{1}{i}(i2).lvl,3),'FontSize',15,...
+                     'Color',[0 0 0],...
+                     'BackgroundColor',[1 1 1],'Margin',1)
             end
         end
     end
