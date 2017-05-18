1e3a8678ed285aaf899f9862908802e03a4a798f
Olivier Lartillot <olartillot@gmail.com>
2017-05-18 08:30:09 +0000

    mirduration: error in ch922 + 'PreSilence' and 'PostSilence' + unit in s.

--- MIRToolbox/mirduration.m
+++ MIRToolbox/mirduration.m
@@ -27,6 +27,16 @@ function varargout = mirduration(orig,varargin)
         minlog.default = 0;
     option.minlog = minlog;    
 
+        presilence.key = 'PreSilence';
+        presilence.type = 'Boolean';
+        presilence.default = 0;
+    option.presilence = presilence;
+
+        postsilence.key = 'PostSilence';
+        postsilence.type = 'Boolean';
+        postsilence.default = 0;
+    option.postsilence = postsilence;
+    
 specif.option = option;
 
 varargout = mirfunction(@mirduration,orig,varargin,nargout,specif,@init,@main);
@@ -35,7 +45,7 @@ varargout = mirfunction(@mirduration,orig,varargin,nargout,specif,@init,@main);
 function [o type] = init(x,option)
 o = mironsets(x,'Attack','Release','Contrast',option.cthr,'Single',option.single,...
                  'Log',option.log,'MinLog',option.minlog,...
-                 'Filter','Normal','AcrossSegments');
+                'Presilence',option.presilence,'PostSilence',option.postsilence);
 type = mirtype(x);
 
 
@@ -43,13 +53,15 @@ function du = main(o,option,postoption)
 if iscell(o)
     o = o{1};
 end
-pa = get(o,'OnsetPos');
+pa = get(o,'AttackPos');
 pr = get(o,'ReleasePos');
+on = get(o,'OnsetPos');
+off = get(o,'OffsetPos');
 d = get(o,'Data');
 t = get(o,'Pos');
-[sl fp] = mircompute(@algo,pa,pr,d,t);
+[sl fp] = mircompute(@algo,pa,pr,d,t,on,off);
 %fp = mircompute(@frampose,pru);
-du = mirscalar(o,'Data',sl,'FramePos',fp,'Title','Duration');
+du = mirscalar(o,'Data',sl,'FramePos',fp,'Title','Duration','Unit','s.');
 du = {du,o};
 
 
@@ -62,7 +74,7 @@ pr = pr{1};
 fp = pr;
 
 
-function [du fp] = algo(pa,pr,d,t)
+function [du fp] = algo(pa,pr,d,t,on,off)
 if isempty(pa)
     du = [];
     fp = [];
@@ -70,20 +82,22 @@ if isempty(pa)
 end
 pa = pa{1};
 pr = pr{1};
+on = on{1};
+off = off{1};
 du = zeros(1,length(pa));
 fp = zeros(2,length(pa));
 for i = 1:length(pa)
-    [mv mp] = max(d(pa(i):pr(2,i)));
+    [mv mp] = max(d(pa(i):pr(i)));
     mp = pa(i) + mp - 1;
-    f1 = find(d(mp:-1:1) < mv * .4,1);
+    f1 = find(d(mp:-1:on(i)) < mv * .4,1);
     if isempty(f1)
         t1 = t(pa(i));
     else
         t1 = t(mp - f1);
     end
-    f2 = find(d(mp:pr(2,i)) < mv * .4,1);
+    f2 = find(d(mp:off(i)) < mv * .4,1);
     if isempty(f2)
-        t2 = t(pr(2,i));
+        t2 = t(pr(i));
     else
         t2 = t(mp + f2);
     end
