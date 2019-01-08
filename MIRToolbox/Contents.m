860d55bd28bb6adb3fe7e1971f38d2424362f339
Olivier Lartillot <olartillot@gmail.com>
2019-01-08 16:17:52 +0000

    MIRtoolbox 1.7.2

--- MIRToolbox/Contents.m
+++ MIRToolbox/Contents.m
@@ -1,5 +1,5 @@
 % MIRtoolbox
-% Version 1.7.1.3 10-December-2018
+% Version 1.7.2 8-January-2019
 %
 % A more detailed documentation of each function is available using the
 % help command. For instance, type help miraudio.
